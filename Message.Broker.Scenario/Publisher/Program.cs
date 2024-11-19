7cf094e820b20cdc66cc413577a6c03159351fd0
mesut <mt3miz@gmail.com>
2024-11-19 21:34:54 +0300

    replaced projects path

---
+++ Message.Broker.Scenario/Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Publisher.Services;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq((rabbitContext, rabbitConfigurator) =>
+    {
+        rabbitConfigurator.Host(new Uri("amqps://swmagxbm:CloaBFaIESN-sqbjKWNh33gu8Ppyb3uB@sparrow.rmq.cloudamqp.com/swmagxbm"));
+    });
+});
+
+builder.Logging.ClearProviders();
+builder.Logging.AddNLog();
+
+builder.Services.AddHostedService<PublishMessageService>(provider =>
+{
+   using IServiceScope serviceScope = provider.CreateScope();
+
+   IPublishEndpoint publishEndpoint = serviceScope.ServiceProvider.GetService<IPublishEndpoint>();
+   
+   var logger = serviceScope.ServiceProvider.GetService<ILogger<PublishMessageService>>();
+
+   return new(publishEndpoint, logger);
+});
+
+var host = builder.Build();
+
+host.Run();
